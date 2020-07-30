4be017887decbb11bb40cba1e210146d825be478
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-07-30 12:37:59 +0100

    added heroku to bind to dollar port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import loadAppResources from "./loaders"
 import { createApp } from "./app"
-import { APP_PORT } from "./config"
+import { PORT } from "./config"
 import http from "http"
 import chalk from "chalk";
  
@@ -13,7 +13,7 @@ loadAppResources(app).then(()=>{
     const Server = http.createServer(app)
     //set up a tls socket
     
-    Server.listen(APP_PORT, () => console.log(chalk.bold.yellow(`server is running at http://localhost:${APP_PORT}`)));
+    Server.listen(PORT, () => console.log(chalk.bold.yellow(`server is running at http://localhost:${PORT}`)));
 })
 
 
