879d621a453f8b337367e3b4a62bb1b84a269b3e
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-08-02 01:26:04 +0100

    solved cert file import bug by deleting it since we dont need to set up ssl

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,16 @@ import loadAppResources from "./loaders"
 import { createApp } from "./app"
 import { PORT } from "./config"
 import http from "http"
-import https from "https"
 import chalk from "chalk";
-import fs from "fs"
-import { IN_PROD } from "./config"
 //create app instance
 const app = createApp();
-const privateKey = fs.readFileSync('cert/server.key', 'utf8');
-const certificate = fs.readFileSync('cert/app.crt', 'utf8');
-const credentials =  {key: privateKey, cert: certificate};
 
 //load app resources
 loadAppResources(app).then(()=>{
     //after app has loaded its resources, succesfully starts the server
     //create httpserver
     const httpServer = http.createServer(app)
-    //create https server
-    //const httpsServer = https.createServer(credentials,app);
+    
     //set up a tls socket
     
     httpServer.listen(PORT, () => console.log(chalk.bold.yellow(`server is running at http://localhost:${PORT}`)));
