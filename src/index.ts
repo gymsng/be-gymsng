174b2e8f4831cc3f74bfc5723a3967b719821347
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-08-03 19:57:26 +0100

    merged upstream with local

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,18 @@ import { createApp } from "./app"
 import { PORT } from "./config"
 import http from "http"
 import chalk from "chalk";
+ 
 //create app instance
 const app = createApp();
 
 //load app resources
 loadAppResources(app).then(()=>{
     //after app has loaded its resources, succesfully starts the server
-    //create httpserver
     const httpServer = http.createServer(app)
-    
-    //set up a tls socket
-    
     httpServer.listen(PORT, () => console.log(chalk.bold.yellow(`server is running at http://localhost:${PORT}`)));
-    
-    
+   
+  
 })
-//Upload the validation file to gymsbackend.herokuapp.com/.well-known/pki-validation/
+
 
  
